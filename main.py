18be2c38a8e1a37b23a80a5ef910f3eb71cbb1b8
fssalazarv <franklinssv@gmail.com>
2025-02-15 19:41:00 +0000

    Eliminando data.xls y actualizando main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from controller.menu import *
+from controller.menu import *git add controller/function.py controller/function.py
 from config.app import *
 from controller.function import *
 if __name__ == "__main__":
